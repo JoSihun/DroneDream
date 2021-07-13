aa76163a5a0fbb4b8d342bb1c8a80f0d2de696be
areum0116 <areum0116@naver.com>
2021-07-13 21:53:01 +0900

    e

--- [05]_본선/Areum_Final_05.m
+++ [05]_본선/Areum_Final_05.m
@@ -2,10 +2,13 @@ clear()
 % HSV Threshold Blue
 thdown_blue = [0.5, 0.35, 0.25];        % 파란색의 임계값 범위
 thup_blue = [0.75, 1, 1];             
-red = [255/255, 0, 0];
-purple = [112/255, 48/255, 160/255];
-th_red = rgb2hsv(red);
-th_purple = rgb2hsv(purple);
+red = rgb2hsv([255/255, 0, 0]);                 % [0, 1, 1]
+purple = rgb2hsv([112/255, 48/255, 160/255]);   % [0.76, 0.7, 0.6]
+thdown_red = [red(1) + 0.1, red(2) - 0.8, red(3) - 0.8];
+thup_red = [red(1) + 0.9, red(2) - 0.3, red(3) - 0.3];
+% th_red = [red(1) + 0.2, red(2) -0.5, red(3) -0.5];
+thdown_purple = [purple(1) - 0.125, purple(2) - 0.4, 0.25];
+thup_purple = [purple(1) + 0.125, purple(2) + 0.3, 1];
 
 % droneObj = ryze();
 % cameraObj = camera(droneObj);
@@ -23,8 +26,9 @@ frame = imread('dot1.jpg');
     % Image Preprocessing
 %     bw_red = (th_red(1) == src_h) & (th_red(2) == src_s) & (th_red(3) == src_v);
 %     bw_purple = (th_purple(1) == src_h) & (th_purple(2) == src_s) & (th_purple(3) == src_v);
-      bw_red = (th_red(1) < src_h)&(src_h < th_red(1) + 0.3) & (th_red(2) - 0.5 < src_s)&(src_s < th_red(2)) & (th_red(3) - 0.5 < src_v)&(src_v < th_red(3));
-      bw_purple = (th_purple(1) - 0.5 < src_h)&(src_h < th_purple(1) + 0.2) & (th_purple(2) - 0.4 < src_s)&(src_s < th_purple(2) + 0.3) & (th_purple(3) - 0.4 < src_v)&(src_v < th_purple(3) + 0.3);
+%       bw_red = (th_red(1) > src_h) & (th_red(2) < src_s) & (th_red(3) < src_v);
+      bw_red = ((thdown_red(1) > src_h)+(src_h > thup_red(1))) & ((thdown_red(2) > src_s)+(src_s > thup_red(2))) & ((thdown_red(3) > src_v)+(src_v > thup_red(3)));
+      bw_purple = (thdown_purple(1) < src_h)&(src_h < thup_purple(1)) & (thdown_purple(2) < src_s)&(src_s < thup_purple(2)) & (thdown_purple(3) < src_v)&(src_v < thup_purple(3));
     
 %     moveforward(droneObj, 'distacne', 1.75);
 %     while(1)
