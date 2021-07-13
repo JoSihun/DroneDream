328299e8c2234a6db2da624bde86aa12d231cbbe
unknown <Administrator@COE517-015.skuce.com>
2021-07-13 15:05:05 +0900

    'test3'

--- [05]_본선/Sihun_Final_02.m
+++ [05]_본선/Sihun_Final_02.m
@@ -6,15 +6,15 @@ thup_green = [0.40, 240/240, 240/240];
 thdown_blue = [0.5, 0.35, 0.25];
 thup_blue = [0.75, 1, 1];
 
-droneObj = ryze();
-cameraObj = camera(droneObj);
+% droneObj = ryze();
+% cameraObj = camera(droneObj);
 % takeoff(droneObj);
-% v = VideoReader('test_video2.mp4');
+v = VideoReader('test_video2.mp4');
 while 1
     % HSV Convert
     disp('HSV Converting');
-%     frame = readFrame(v);
-    frame = snapshot(cameraObj);
+    frame = readFrame(v);
+%     frame = snapshot(cameraObj);
     src_hsv = rgb2hsv(frame);
     src_h = src_hsv(:,:,1);
     src_s = src_hsv(:,:,2);
@@ -24,11 +24,11 @@ while 1
     % Image Preprocessing
     bw1 = (0.5 < src_h)&(src_h < 0.75) & (0.15 < src_s)&(src_s < 1) & (0.25 < src_v)&(src_v < 1);   % 파란색 검출
     
+        
     
     
-    
-    
-    bw2 = imfill(bw1,'holes');                                                                      % 구멍을 채움
+    bw2 = imfill(bw1,'holes');
+    imshow(bw2)% 구멍을 채움
     %구멍을 채우기 전과 후를 비교하여 값이 일정하면 0, 변했으면 1로 변환
     for row = 1:rows
         for col = 1:cols
@@ -57,12 +57,12 @@ while 1
     camera_mid_row = rows / 2;
     camera_mid_col = cols / 2;
     
-    subplot(2, 2, 1), imshow(frame); hold on;
-    plot(center_col, center_row, 'r*'); hold off;
-    subplot(2, 2, 3), imshow(bw1); hold on;
-    plot(center_col, center_row, 'r*'); hold off;
-    subplot(2, 2, 4), imshow(bw2); hold on;
-    plot(center_col, center_row, 'r*'); hold off;
+%     subplot(2, 2, 1), imshow(frame); hold on;
+%     plot(center_col, center_row, 'r*'); hold off;
+%     subplot(2, 2, 3), imshow(bw1); hold on;
+%     plot(center_col, center_row, 'r*'); hold off;
+%     subplot(2, 2, 4), imshow(bw2); hold on;
+%     plot(center_col, center_row, 'r*'); hold off;
 %     imshow(bw1);
 %     imshow(bw2);
     
